e8251c835a55333f74d09afe387eb95a8c8e2ccf
youtubechannelcodify-hub <youtubechannelcodify@gmail.com youtubechannelcodify-hub>
2026-08-20 01:36:38 +0000

    fix: use official Cloudflare Pages SPA mode

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.scss"; 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom"; 
 import DarkModeContextProvider from "./context/darkModeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <DarkModeContextProvider>
       <App />
     </DarkModeContextProvider>
-  </HashRouter> 
+  </BrowserRouter> 
 );
