3e6bb13fd907b0cf54f9ed5170b5333caf08783b
youtubechannelcodify-hub <youtubechannelcodify@gmail.com youtubechannelcodify-hub>
2026-08-20 00:11:22 +0000

    fix: revert build script, keep homepage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./style.scss";
-import { BrowserRouter } from "react-router-dom";
+import "./style.scss"; 
+import { HashRouter } from 'react-router-dom';
 import DarkModeContextProvider from "./context/darkModeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <DarkModeContextProvider>
       <App />
     </DarkModeContextProvider>
-  </BrowserRouter>,
+  </HashRouter> 
 );
